a443d5e336f48139ac5a994c9e19eee79ec24ee8
Shan Jiang <jiangshan789123@hotmail.com>
2023-10-16 20:37:07 -0400

    created folders for dev and binaries

--- client.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-# client side has only one socket
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(("127.0.0.1", 1234))
-    s.sendall(b"Hello, Ross!")
-    data = s.recv(1024)
-    print("Received: ", repr(data))
