83d636c68944dfeb0dd1d30268ac02c3727d9c06
Shan Jiang <jiangshan789123@hotmail.com>
2023-11-09 19:45:39 -0500

    small changes

--- XMOS/dev/copyAndCompile.sh
+++
@@ -1,8 +0,0 @@
-cp audiohw_ref.xc ../testing/sw_usb_audio/app_usb_aud_xk_316_mc/src/extensions/audiohw.xc
-cd ../testing/sw_usb_audio/app_usb_aud_xk_316_mc && pwd && \
-	xmake CONFIG=2AMi8o8xxxxxx && \
-	cd bin && cd 2AMi8o8xxxxxx && \
-	cp app_usb_aud_xk_316_mc_2AMi8o8xxxxxx.xe ../../../../../bin
-
-
-
