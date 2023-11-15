b0cdf0497623565bbe551cc03f4594d02629a79c
Shan Jiang <jiangshan789123@hotmail.com>
2023-11-15 17:25:00 -0500

    add the `and` mark to copyAndCompile.sh

--- XMOS/dev/copyAndCompile_win.sh
+++ XMOS/dev/copyAndCompile_win.sh
@@ -1,7 +1,7 @@
 # copy the modified file to overwrite its corresponding file
-cp xk-audio-316-mc_nov_9.xn ../testing/sw_usb_audio/app_usb_aud_xk_316_mc/src/core/xk-audio-316-mc.xn
-cp xua_conf_nov_9.h ../testing/sw_usb_audio/app_usb_aud_xk_316_mc/src/core/xua_conf.h
-cp audiohw_nov_9.xc ../testing/sw_usb_audio/app_usb_aud_xk_316_mc/src/extensions/audiohw.xc
+cp xk-audio-316-mc_nov_9.xn ../testing/sw_usb_audio/app_usb_aud_xk_316_mc/src/core/xk-audio-316-mc.xn && \
+cp xua_conf_nov_9.h ../testing/sw_usb_audio/app_usb_aud_xk_316_mc/src/core/xua_conf.h && \
+cp audiohw_nov_9.xc ../testing/sw_usb_audio/app_usb_aud_xk_316_mc/src/extensions/audiohw.xc && \
 # go to the root and compile
 cd ../testing/sw_usb_audio/app_usb_aud_xk_316_mc && pwd && \
 	xmake CONFIG=2AMi8o8xxxxxx && \
