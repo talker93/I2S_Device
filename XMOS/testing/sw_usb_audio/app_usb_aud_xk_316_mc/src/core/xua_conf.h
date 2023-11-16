a80e371e6a27e5602ccb0bb4a1a75404b83cd44a
Shan Jiang <jiangshan789123@hotmail.com>
2023-11-16 15:40:30 -0500

    compiled

--- XMOS/testing/sw_usb_audio/app_usb_aud_xk_316_mc/src/core/xua_conf.h
+++ XMOS/testing/sw_usb_audio/app_usb_aud_xk_316_mc/src/core/xua_conf.h
@@ -137,8 +137,8 @@
 #ifndef PID_AUDIO_1
 #define PID_AUDIO_1        (0x0017)
 #endif
-#define PRODUCT_STR_A2     "XMOS xCORE.ai MC (UAC2.0)"
-#define PRODUCT_STR_A1     "XMOS xCORE.ai MC (UAC1.0)"
+#define PRODUCT_STR_A2     "Biomedical Microsystems Laboratory - ADC module"
+#define PRODUCT_STR_A1     "Biomedical Microsystems Laboratory - ADC module"
 
 /* Board power source - Default is bus-powered */
 #ifndef XUA_POWERMODE
