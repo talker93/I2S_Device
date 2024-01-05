af25644b3b3475062fb5bcd9afa945e91be72722
Shan Jiang <jiangshan789123@hotmail.com>
2024-01-05 17:06:26 -0500

    return to audiohw_nov_9 version, which is TLV320 enabled.

--- XMOS/dev/copyAndCompile_win_ref.sh
+++ XMOS/dev/copyAndCompile_win_ref.sh
@@ -1,7 +1,7 @@
 # copy the modified file to overwrite its corresponding file
 cp ./ref/xk-audio-316-mc.xn ../testing/sw_usb_audio/app_usb_aud_xk_316_mc/src/core/xk-audio-316-mc.xn && \
 cp ./ref/xua_conf.h ../testing/sw_usb_audio/app_usb_aud_xk_316_mc/src/core/xua_conf.h && \
-cp ./ref/audiohw_ref.xc ../testing/sw_usb_audio/app_usb_aud_xk_316_mc/src/extensions/audiohw.xc && \
+cp ./audiohw_nov_9.xc ../testing/sw_usb_audio/app_usb_aud_xk_316_mc/src/extensions/audiohw.xc && \
 cp Makefile ../testing/sw_usb_audio/app_usb_aud_xk_316_mc/Makefile && \
 cp main_dev.xc ../testing/lib_xua/lib_xua/src/core/main.xc && \
 # go to the root and compile
